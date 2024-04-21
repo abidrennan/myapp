3e865330302ceebc60195a7c15b77e3ee7eb89e5
Abi Drennan <adrennan@uccs.edu>
2024-04-20 21:06:25 -0600

    Define URI path and view

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo_app',
+]
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
 ]
 
 MIDDLEWARE = [
