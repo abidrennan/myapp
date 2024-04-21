9498b55a317b64532be21a60b1d759dd93eaa729
Abi Drennan <adrennan@uccs.edu>
2024-04-21 02:50:51 -0600

    templates, urls, views

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
 from . import views
+from .views import SkillList, SkillDetail, SkillCreate, SkillUpdate, SkillDelete, SkillLogin
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('skills/', SkillList.as_view(), name='skill-list'),
+    path('skill/<int:pk>/', SkillDetail.as_view(), name='skill'),
+    path('skill-create/', SkillCreate.as_view(), name='skill-create'),
+    path('skill-update/<int:pk>/', SkillUpdate.as_view(), name='skill-update'),
+    path('skill-delete/<int:pk>/', SkillDelete.as_view(), name='skill-delete'),
+    path('login/', SkillLogin.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,6 +1,56 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Skill
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # Create your views here.
 def index(request):
     return render(request, 'todo_app/index.html')
+
+class SkillList(LoginRequiredMixin, ListView):
+    model = Skill
+    context_object_name = 'skills'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['count'] = context['skills'].filter(complete=False).count()
+        return context
+
+class SkillDetail(LoginRequiredMixin, DetailView):
+    model = Skill
+    context_object_name = 'skill'
+
+class SkillCreate(LoginRequiredMixin, CreateView):
+    model = Skill
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy("skill-list")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(SkillCreate, self).form_valid(form)
+    
+class SkillUpdate(LoginRequiredMixin, UpdateView):
+    model = Skill
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('skill-list')
+
+class SkillDelete(LoginRequiredMixin, DeleteView):
+    model = Skill
+    context_object_name = 'skill'
+    success_url = reverse_lazy('skill-list')
+    template_name = 'todo_app/delete_skill.html'
+
+class SkillLogin(LoginView):
+    template_name = 'todo_app/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('skill-list')
